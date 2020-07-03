46a4fd60aed78eac9ae6337422264d8fd697a5cc
Asiegbu Miracle Kanu-Asiegbu <akanu@strings.engin.umich.edu>
2020-07-03 10:39:58 -0400

    10 layers lstm

--- fine_tune_lstm.py
+++ fine_tune_lstm.py
@@ -174,11 +174,17 @@ def model1(x_train,y_train, x_test,y_test):
     model = keras.models.Sequential()
     model.add(keras.layers.InputLayer(input_shape=x_train.shape[-2:]))
 
-    model.add(keras.layers.LSTM({{choice([1, 2, 3,4,5,6])}},return_sequences =True ) )
-    model.add(keras.layers.LSTM({{choice([1, 2, 3,4,5,6])}},return_sequences =True ) )
-    model.add(keras.layers.LSTM({{choice([1, 2, 3,4,5,6])}},return_sequences =True ) )
-    model.add(keras.layers.LSTM({{choice([1, 2, 3,4,5,6])}},return_sequences =True ) )
-    model.add(keras.layers.LSTM({{choice([1, 2, 3,4,5,6])}},return_sequences =True ) )
+    model.add(keras.layers.LSTM({{choice([1, 2, 3,4,5,6,7,8,9,10,11,12])}},return_sequences =True ) )
+    model.add(keras.layers.LSTM({{choice([1, 2, 3,4,5,6,7,8,9,10,11,12])}},return_sequences =True ) )
+    model.add(keras.layers.LSTM({{choice([1, 2, 3,4,5,6,7,8,9,10,11,12])}},return_sequences =True ) )
+    model.add(keras.layers.LSTM({{choice([1, 2, 3,4,5,6,7,8,9,10,11,12])}},return_sequences =True ) )
+    model.add(keras.layers.LSTM({{choice([1, 2, 3,4,5,6,7,8,9,10,11,12])}},return_sequences =True ) )
+    model.add(keras.layers.LSTM({{choice([1, 2, 3,4,5,6,7,8,9,10,11,12])}},return_sequences =True ) )
+    model.add(keras.layers.LSTM({{choice([1, 2, 3,4,5,6,7,8,9,10,11,12])}},return_sequences =True ) )
+    model.add(keras.layers.LSTM({{choice([1, 2, 3,4,5,6,7,8,9,10,11,12])}},return_sequences =True ) )
+    model.add(keras.layers.LSTM({{choice([1, 2, 3,4,5,6,7,8,9,10,11,12])}},return_sequences =True ) )
+    model.add(keras.layers.LSTM({{choice([1, 2, 3,4,5,6,7,8,9,10,11,12])}},return_sequences =True ) )
+
 
 
     model.add(keras.layers.LSTM(4) )
